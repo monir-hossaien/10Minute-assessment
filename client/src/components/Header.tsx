9acf76b6f3998413d692b418c7f56b7cabeba336
Monir Hossain <monirhdigital@gmail.com>
2025-07-28 06:20:02 +0600

    header content update

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -10,10 +10,10 @@ const Header = ({value}: Props) => {
     return (
         <div className="container px-4 md:px-0">
             <div className="space-y-4 md:space-y-5">
-                <h1 className="text-2xl md:text-4xl font-semibold leading-snug md:leading-[40px]">
+                <h1 className="text-xl md:text-3xl font-semibold leading-snug md:leading-[40px]">
                     {value?.title}
                 </h1>
-                <div className="w-1/2 text-sm md:text-base font-normal leading-[24px] text-[#a3a3a3]">
+                <div className="w-full md:w-1/2 text-sm md:text-base font-normal leading-[24px] text-[#a3a3a3]">
                     {parse(value?.description)}
                 </div>
             </div>
